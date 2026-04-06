92b980af264f8b151bde8f79df40c186efb8a5c5
DevinSegovia <alumno@et26.local>
2026-04-06 17:24:26 -0300

    java script

---
+++ index.js
@@ -0,0 +1,6 @@
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Noviembre", "Diciembre"]
+console.log(meses);
+for (var i = 0; i < meses.length; i++){
+    document.writeln("<h1>" + meses[i] + "<(h1)>");
+}
